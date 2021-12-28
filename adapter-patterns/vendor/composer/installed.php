71584e366422b1311b9bf2bc9a9a30f3e9eefcf6
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-12-28 22:42:15 +0530

    Created composer file and initialize it with autoloading

---
+++ adapter-patterns/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8dd23294db76f060e56347ead92ebc5186cb2de1',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8dd23294db76f060e56347ead92ebc5186cb2de1',
+    ),
+  ),
+);
